df92a2b7ed3e78a4a886924a74f2b85d380b081a
efrat5454 <efrat5454@gmail.com>
2022-06-26 20:25:41 +0300

    Add rating and score

--- src/components/test/Test.js
+++ src/components/test/Test.js
@@ -1,9 +1,11 @@
-
-function Test(){
-    return <>
-    
+import RatingAndScore from "../../common/ReatingAndScore";
+import "./Test.css";
+function Test() {
+  return (
+    <>
+      <RatingAndScore wpm={120} std={5} compScore={9} />
     </>
+  );
 }
 
-
-export default Test
+export default Test;
