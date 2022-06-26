96c6490a720f58cb22eaef03aea96bc4c91dbc31
נהוראי <nehoraimimon@gmail.com>
2022-06-26 19:25:08 +0300

    new brunch

--- src/pages/Dashboard/FunctionBtn.js
+++ src/pages/Dashboard/FunctionBtn.js
@@ -1,9 +1,11 @@
-export default function FunctionBtn(props){
-    return <>
-    //lot of errors 
-    {props.name}
-    {if ({props.name} === "Weekly Assessment"){
-        <div>`access in {/* days left*/} days`</div>}
-    
+import styles from './style.module.css'
+export default function FunctionBtn(props) {
+  return (
+    <>
+      <div className={`${styles.word}`}>{props.name}</div>
+      {props.name === "Weekly Assessment" && (
+        <div className={`${styles.word}`}>`access in {/* days left*/} days`</div>
+      )}
     </>
+  );
 }
