45e3b7459ed9fde6e3fa7caf211ef940e359b23a
yishai <harelity@gmail.co.il>
2022-06-27 12:45:38 +0300

    ver3

--- src/components/common/DashboardBox/Box1.js
+++ src/components/common/DashboardBox/Box1.js
@@ -1,28 +1,41 @@
-import "./box.css"
-import icon1 from './icon1.png';
-import icon2 from './icon2.png';
-import icon3 from './icon3.png';
-import icon4 from './icon4.png';
-function Box1(){
-const button=[{ name:'Train Reading',icon:icon1},{name:'Train Focus',icon:icon2},{name:'Weekly Assessment',icon:icon3},{name:'Freestyle',icon:icon4}]
-    // yishai
-        return <> {button.map((v=>{console.log("efef")}))}
-        <div className =" box  boxbehind" ></div>
-    <div className ="box box1">
-<div className ="icon1" ><img src={icon1} alt="book"  />  </div>
-      
-        
-        <div className ="namebox">stresss
-        
+import "./box.css";
+import icon1 from "./icon1.png";
+import icon2 from "./icon2.png";
+import icon3 from "./icon3.png";
+import icon4 from "./icon4.png";
+function Box1() {
+  const button = [
+    { name: "Train Reading", icon: icon1 },
+    { name: "Train Focus", icon: icon2 },
+    { name: "Weekly Assessment", icon: icon3 },
+    { name: "Freestyle", icon: icon4 },
+  ];
+  // yishai
+  return (
+    <>
+     
+      <div className="grid-container">
+        <>
+      {button.map((v) => {
+                return (
+          <div>
+            <div className=" box  boxbehind"></div>
+            <div className="box box1" onClick={(e) => console.log("df")/* props.func(v.name) */ }>
+              <div className="icon1">
+                <img src={v.icon} alt="book" />{" "}
+              </div>
+
+              <div className="namebox">{v.name}</div>
+            </div>
+          </div>
+          
+          );
+        })}
+    </>
+
         </div>
-         </div>
-        
-         
-         
-         
-        </>
-    
+    </>
+  );
 }
 
-
-export default Box1
+export default Box1;
--- src/components/test/Test.js
+++ src/components/test/Test.js
@@ -1,4 +1,4 @@
- import Box2 from '../common/DashboardBox/Box2';  
+  
  import Box1 from '../common/DashboardBox/Box1'; 
 function Test(){
 
