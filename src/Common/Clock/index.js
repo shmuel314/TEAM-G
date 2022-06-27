966805291adbea1f13bcb33eaa8bd8614814602a
milka menachem <milka.elimelech@mail.huji.ac.il>
2022-06-27 09:57:11 +0300

    clock+time left

--- src/Common/Clock/index.js
+++ src/Common/Clock/index.js
@@ -4,44 +4,32 @@ import { CountdownCircleTimer } from "react-countdown-circle-timer";
 
 import "./style.module.css";
 
-function Clock() {
-  const renderTime = ({ remainingTime }) => {
-    if (remainingTime === 0) {
-      return <div className="timer">Too lale...</div>;
-    }
+const renderTime = ({ remainingTime }) => {
+  const minutes = Math.floor(remainingTime / 60);
+  const seconds = remainingTime - minutes * 60;
 
-    return (
-      <div className="timer">
-        <div className="text">Remaining</div>
-        <div className="value">{remainingTime}</div>
-        <div className="text">seconds</div>
-      </div>
-    );
-  };
   return (
-    <div className="App">
-      <h1>
-        CountdownCircleTimer
-        <br />
-        React Component
-      </h1>
-      <div className="timer-wrapper">
-        <CountdownCircleTimer
-          isPlaying
-          duration={10}
-          colors={["#004777", "#F7B801", "#A30000", "#A30000"]}
-          colorsTime={[10, 6, 3, 0]}
-          onComplete={() => ({ shouldRepeat: true, delay: 1 })}
-        >
-          {renderTime}
-        </CountdownCircleTimer>
+    <div className="timer">
+      <div className="value">
+        {minutes}:{seconds}
       </div>
-      <p className="info">
-        Change component properties in the code filed on the right to try
-        difference functionalities
-      </p>
+    </div>
+  );
+};
+
+function Clock() {
+  return (
+    <div className="timer-wrapper">
+      <CountdownCircleTimer
+        isPlaying
+        duration={180}
+        colors={["#7D56A5"]}
+        onComplete={() => ({ shouldRepeat: true, delay: 1 })}
+      >
+        {renderTime}
+      </CountdownCircleTimer>
     </div>
   );
 }
 
-export default { Clock };
+export default Clock;
