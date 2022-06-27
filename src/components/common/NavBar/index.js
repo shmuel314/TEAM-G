9cdd87e660067e2aeeb02604346f4033422b0210
shmuel asherov <shmuelas7@gmail.com>
2022-06-27 10:32:54 +0300

    10:32

--- src/components/common/NavBar/index.js
+++ src/components/common/NavBar/index.js
@@ -1,7 +1,11 @@
 import styles from "./style.module.css";
 
 import { BsColumnsGap } from "react-icons/bs";
+import { FiSettings } from "react-icons/fi";
+import { MdLogout } from "react-icons/md";
+import { FaRegAddressBook } from "react-icons/fa";
 import { NavButton } from "../NavButton/NavButton";
+
 import { NavHead } from "../NavHead";
 
 export const NavBar = () => {
@@ -11,9 +15,30 @@ export const NavBar = () => {
       <ul>
         <li className={`${styles.btn}`}>
           <NavButton
-            icon={<BsColumnsGap className={`${styles.icon}`} />}
+            icon={<BsColumnsGap className={`${styles.icon}`} size="30px" />}
             text="Dashboard"
-            link=""
+            link="Dashboard"
+          />
+        </li>
+        <li className={`${styles.btn}`}>
+          <NavButton
+            icon={<FiSettings className={`${styles.icon}`} size="30px" />}
+            text="Settings"
+            link="Settings"
+          />
+        </li>
+        <li className={`${styles.btn}`}>
+          <NavButton
+            icon={<FaRegAddressBook className={`${styles.icon}`} size="30px" />}
+            text="Contact Us"
+            link="Contact Us"
+          />
+        </li>
+        <li className={`${styles.btn}`}>
+          <NavButton
+            icon={<MdLogout className={`${styles.icon}`} size="30px" />}
+            text="Sign Out"
+            link="Sign Out"
           />
         </li>
       </ul>
