1f2ff3881e0e629109dab0f3f63ebcb3295ac162
zvilior <102918963+zvilior@users.noreply.github.com>
2022-06-27 14:09:08 +0300

    14:08

---
+++ src/pages/Assessment/Page3/index.js
@@ -0,0 +1,16 @@
+import "./style.css"
+
+import React from 'react'
+
+function index() {
+    return (header - "camera set-up"
+
+  'camera Setup'
+
+    'start button' 'image'
+
+        < div > index</div >
+  )
+}
+
+export default index
