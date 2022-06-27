af78d7ddb1d81f9316d0cbd5d8530d0678a482c6
daniel nagar <danielnagar80@gmail.com>
2022-06-27 09:33:31 +0300

    aaa

---
+++ src/components/common/titleRead/index.js
@@ -0,0 +1,11 @@
+import styles from './style.module.css'
+
+// Creator : Team A - Yehonatan & Daniel
+function TitleRead(){
+    return <div className={styles.titelRead}>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi, corrupti!
+    </div>
+
+}
+
+export default TitleRead;
