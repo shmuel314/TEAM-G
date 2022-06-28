0a729fa069021bdd586e5c1c58e1bdaa8d01a1cc
shmuel harel <shmuelharel10@gmail.com>
2022-06-28 17:20:39 +0300

    pull itay2

--- src/pages/TrainFocus/Exercise/index.js
+++ src/pages/TrainFocus/Exercise/index.js
@@ -5,7 +5,7 @@
 import { useContext, useState} from 'react'
 import {pageNameContext} from "../../../components/layout/Layout.js"
 import "./style.css"
-import arow from "./arow.png"
+// import arow from "./arow.png"
 import {useNavigate} from 'react-router-dom'
 import SabmitBtn from '../../../components/common/SubmitBtn'
 
@@ -46,7 +46,8 @@ function Exercise(){
         return <>
         <div className="purpleDot"></div>
         <div className="back_center_Left"> <div className="text_box">Move the phone a bit to the left so the dot is in your center</div> 
-        <img  className="flip_img_left"src={arow} alt="img"/></div> 
+        {/* <img  className="flip_img_left"src={arow} alt="img"/> */}
+        </div> 
         <SabmitBtn name="stop" path={RoundNumber<5? '/train-focus/StartFocus': '/train-focus/result'} onclick={Round}/>
         </>
        break;
@@ -54,7 +55,8 @@ function Exercise(){
         return <>
         <div className="purpleDot"></div>
             <div className="back_center_Right"> <div className="text_box">Move the phone a bit to the right so the dot is in your center</div>
-            <img  className="flip_img_right"src={arow} alt="img"/></div>
+            {/* <img  className="flip_img_right"src={arow} alt="img"/> */}
+            </div>
             <SabmitBtn name="stop" path={RoundNumber<5? '/train-focus/StartFocus': '/train-focus/result'} onclick={Round}/>
            </>
        break;
