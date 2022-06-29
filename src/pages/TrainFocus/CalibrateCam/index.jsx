d302f6abb4ca89d3ebbaeff16fcb8a53c061ea81
Orit Flint <orelflint@gmail.com>
2022-06-29 16:12:55 +0300

    finish my page

--- src/pages/TrainFocus/CalibrateCam/index.jsx
+++ src/pages/TrainFocus/CalibrateCam/index.jsx
@@ -1,82 +1,51 @@
 import './Style.css'
-import React, {  useState, useRef } from 'react';
-import DashboardBtn from '../../../components/common/DashboardBtn';
-import { useNavigate } from 'react-router-dom';
+import React, {  useState, useRef, useEffect, useContext } from 'react';
 import SubmitBtn from '../../../components/common/SubmitBtn';
-
+import moveDot from '../../../assets/img/trainFocus/moveDot.png'
+// import {pageNameContext} from '../../../components/layout/Layout'
 
   
 // Creator : Team G - Orit F  
 function CalibrateCam(){
-    const halfImgWidth = 50
-    const rightMargin = halfImgWidth-30
-    const leftMargin = halfImgWidth+30
-    //const userLogic = require('../BL/userBL')
-  // const dragItem = useRef()
-  const [posX, setPosX] = useState(window.innerWidth/2-halfImgWidth);
-  const navigate = useNavigate();
+  // const [setShowHeader] = useContext(pageNameContext)
+const imgMiddleWidth = 50 //total icon width is ~100px. to place it in center using "left" style we need to ident it 50px left
+const leftMargin = 80
 
-// const drag = (e) => {
-//         setPosX(e.clientX-halfImgWidth);
-//   };
-
-//   const dragEnd = (e) => {
-//     setPosX(e.clientX-halfImgWidth);
-//   };
+  const [posX, setPosX] = useState(window.innerWidth/2-imgMiddleWidth);
 
   const touch = (e) => {
-    if(e.changedTouches[0].clientX<=0){
-        setPosX(0-rightMargin)
+    let total = (window.innerWidth-leftMargin)
+    if(posX<0 && posX!=-20){
+        setPosX(-20)
         return false;
     }
-    else if(e.changedTouches[0].clientX >= window.innerWidth){
-        let total = (window.innerWidth-leftMargin)
+    else if(posX > total){       
         setPosX(total)
         return false;
     }
     else
-        setPosX(e.changedTouches[0].clientX-halfImgWidth); 
-  };
-
-  
-  const touchEnd = (e) => {
-    console.log("window.innerWidth"+window.innerWidth)
-    if(e.changedTouches[0].clientX<=0){
-        setPosX(0-rightMargin)
-        return false;
-    }
-    if(e.changedTouches[0].clientX >= window.innerWidth){
-        let total = (window.innerWidth-leftMargin)
-        //console.log("bigger then width",total)
-        setPosX(total)
-        return false;
-    }
-    setPosX(e.changedTouches[0].clientX-halfImgWidth);
+        setPosX(e.changedTouches[0].clientX-imgMiddleWidth); 
   };
 
   const onClickBtn=()=>{
-    localStorage.setItem('posX', posX);
-    console.log("send posX to DB. current posX="+posX)
-     navigate('/train-focus/StartFocus');
-    //userLogic.setCalibrateCam(mobileID, posX)
+      localStorage.setItem('cameraPosX', posX);
   }
+
+  useEffect(()=>{
+    // setShowHeader(false)
+  },[]);
 
    return (
     <div className='calibrateCam'>
-        <div className='arrowMove2' >
-            <div className='place' style={{left:posX+'px'}}
-                onTouchMove={(e) => touch(e)}
-                onTouchEnd={(e) => touchEnd(e)}
-                draggable="false">
-                <img className='dragImg' src={require("./moveDot.png")} alt=""/>
-            </div>  
-        </div>       
+        <div className='place' style={{left:posX+'px'}}
+            onTouchMove={(e) => touch(e)}
+            onTouchEnd={(e) => touch(e)}>
+            <img className='dragImg' src={moveDot} alt=""/>
+        </div>      
         <div className='arrowMove'>
             <span>Slide the arrow left/right so that it points at your camera</span>
         </div>
-      {/* <DashboardBtn /> */}
-      {/* <SubmitBtn name="continue" path="/train-focus/StartFocus" onclick={onClickBtn} /> */}
-      <button id="btnContinue" onClick={onClickBtn} className="continueBtn">continue</button>
+        <SubmitBtn name="continue" path="/train-focus/StartFocus" onclick={onClickBtn} />
     </div>
    );
 }
